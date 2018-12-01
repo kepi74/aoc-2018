0a0fdbefc6771b2a5a6ddc3aeb9697415d0d0624
Pavel Kepka <pkepka@blueberryapps.com>
2018-12-02 00:17:57 +0100

    Day 1b solution

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject aoc-2018 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Advent of Code 2018"
+  :url "https://github.com/kepi74/aoc-2018"
+  :main aoc-2018.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
--- src/aoc_2018/core.clj
+++ src/aoc_2018/core.clj
@@ -1,6 +1,21 @@
-(ns aoc-2018.core)
+(ns aoc-2018.core
+    (:require [aoc-2018.days :as days]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn get-final-freq
+  "Day one: calculate resulting frequency"
+  [freq_list]
+  (reduce + freq_list))
+
+(defn repeated-freq [freq_list]
+  (reduce
+    (fn [vect freq] (
+        let [curr_freq (+ (last vect) freq)
+            new_vect (conj vect curr_freq)]
+        (if (true? (some #(= curr_freq %) vect)) (reduced (last new_vect)) new_vect)))
+        ;;(if (= curr_freq 590) (reduced new_vect) new_vect)))
+    [0]
+    (cycle freq_list)))
+
+(defn -main []
+  (println (get-final-freq days/day_01))
+  (println (repeated-freq days/day_01)))
